62b0b50f21f9131ddd55e54498a1f3384cb2e7e1
HectorSanchez <hsanchez@netrixllc.com>
2024-05-27 16:36:55 -0300

    fix: typo fixed and test are on going

--- streamlit_app/src/pages/🦾_Prompt_Tester.py
+++ streamlit_app/src/pages/🦾_Prompt_Tester.py
@@ -2,27 +2,28 @@ import streamlit as st
 from few_shot import build_prompt
 import boto3
 
-def main():
-    st.set_page_config(
+st.set_page_config(
     page_title="Prompt Tester",
     page_icon="🦾",
-    )
-    st.title("Bienvenido 👋")
-    st.markdown(
+)
+st.title("Prompt Tester 🦾")
+st.markdown(
     """
     Aqui vas a poder observar los prompt templates que se utilizan dependiendo el ticket 
     seleccionado
     """
-    )
+)
+def main():
     bedrock = boto3.client(region_name= 'us-east-1', service_name='bedrock-runtime')
     with st.form('my_form'):
-        text = st.text_area('Enter text:', ' ')
+        text = st.text_area(label='Enter text:', value=' ')
+        st.write('You entered:', text)
         submitted = st.form_submit_button('Submit')
-    
+        
         if submitted:
             promptTemplate = build_prompt(text, bedrock)
             prompt = promptTemplate.generate_prompt()
-            st.write(prompt.format(text))
+            st.write(prompt.format(doc_text=text))
 
 if __name__ == '__main__': 
     main()
